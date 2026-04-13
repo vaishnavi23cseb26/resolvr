6180a4ffa3eb711253390d95883a72bcb7c6ab1d
Vaishnavi <23cseb26vaishnavi@gmail.com>
2026-04-13 22:41:35 +0530

    fix port binding for render

--- server/server.js
+++ server/server.js
@@ -106,14 +106,13 @@ const PORT = process.env.PORT || 5000;
   try {
     await connectDB();
     initCloudinary();
-    server.listen(PORT, () => {
-      // eslint-disable-next-line no-console
+
+    server.listen(PORT, "0.0.0.0", () => {
       console.log(`Server running on port ${PORT}`);
     });
+
   } catch (err) {
-    // eslint-disable-next-line no-console
     console.error(err);
     process.exit(1);
   }
 })();
-
